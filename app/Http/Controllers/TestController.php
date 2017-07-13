25c982141c3bbf67428e419f97a6e3e3035f0e16
Femi Oladele <profemzy@gmail.com>
2017-07-13 09:15:37 +0100

    Locked Down All Important Controllers

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('test.index');
